b9c1848c385d27de618f96664e232b73b86529e5
藤原真子 <makofujihara@fujiwaramasakonoMacBook-Air.local>
2026-05-13 12:05:23 +0900

    ログイン機能追加

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,19 @@
+class UserSessionsController < ApplicationController
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_to root_path, notice: 'ログインしました'
+    else
+      flash.now[:alert] = 'ログインに失敗しました'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, status: :see_other, notice: 'ログアウトしました'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
   root 'static_pages#top'
 
   # ユーザー登録用
@@ -11,4 +14,11 @@ Rails.application.routes.draw do
   # ログイン関係（後で実装する際の器だけ作っておく）
   # 現時点ではエラーを避けるため、とりあえず root などに飛ばす設定でもOK
   get 'login', to: 'static_pages#top', as: :login 
+
+  # ログイン画面
+get 'login', to: 'user_sessions#new'
+# ログイン実行
+post 'login', to: 'user_sessions#create'
+# ログアウト
+delete 'logout', to: 'user_sessions#destroy'
 end
